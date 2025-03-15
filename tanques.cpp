0be21c871afbd610cbf4100251d2b16e08b84732
SMagob <danielemoranv@gmail.com>
2025-03-15 14:50:26 -0400

    first commit

---
+++ tanques.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout << "Codigo desarrollado por Daniel Moran en la Universidad Nacional Experimental Politecnica de Venezuela \n"<< endl;
+	return 0;
+}
