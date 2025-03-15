664a2f35a0bcdfae13ac95e4a34794b1b435400d
Miruto80 <miguelfernando8012@gmail.com>
2025-03-15 15:48:45 -0400

    Pueh

--- tanques.cpp
+++ tanques.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main(){
 	cout << "Codigo desarrollado por Daniel Moran en la Universidad Nacional Experimental Politecnica de Venezuela \n"<< endl;
+	cout <<"prueba";
 	return 0;
 }
