e7415bf1a08a4809e49b01e8bb5a19d1ac27cb01
SMagob <danielemoranv@gmail.com>
2025-03-16 00:52:16 -0400

    a'

--- tanques.cpp
+++ tanques.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
 using namespace std;
 
+class tanquepetroleo {
+	private:
+	int codigo;
+	float nivelliquido, temperatura, presion, densidad, nivelagua;
+}
 
+class sensor: public tanquepetroleo{
+	public:
+	medirnivelliquido(){
+
+	}
+	medirtemperatura(){
+
+	}
+	medirpresion(){
+
+	}
+	medirdensidad(){
+
+	}
+	medirnivelagua(){
+		
+	}
+}
 
 int main(){
 	cout << "Codigo desarrollado por Daniel Moran en la Universidad Nacional Experimental Politecnica de Venezuela \n"<< endl;
