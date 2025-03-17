f9a769b22bc7c365be8a1a42774813f1976f874a
RonKnox <rggalvarez@gmail.com>
2025-03-17 17:25:27 -0300

    Revert "Merge branch 'main' of https://github.com/SMagob/tanques-de-petroleo"
    This reverts commit 58eb78a6a591c6f6a146d96d132bc7b3f02dbe11, reversing
    changes made to b1c322809b8028827c5ea995519e4a6aae98641f.

--- tanques.cpp
+++ tanques.cpp
@@ -36,10 +36,6 @@ class alarma{
 };
 
 int main(){
-<<<<<<<<< Temporary merge branch 1
-	cout << "Codigo desarrollado por Daniel Moran en la Universidad Nacional Experimental Politecnica de Venezuela a\n"<< endl;
-=========
-	cout << "Codigo desarrollado por Daniel Moran, Jesus Morales en la Universidad Nacional Experimental Politecnica de Venezuela \n"<< endl;
->>>>>>>>> Temporary merge branch 2
+	cout << "Codigo desarrollado por Daniel Moran, Jesus Morales, Ronald Gordillo en la Universidad Nacional Experimental Politecnica de Venezuela \n"<< endl;
 	return 0;
 }
