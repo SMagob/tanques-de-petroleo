42cebb5f73a90c234b342ddabc1a5c41b53a262e
SMagob <danielemoranv@gmail.com>
2025-03-24 01:32:59 -0400

    ahora si Zzzzz

--- tanques.cpp
+++ tanques.cpp
@@ -201,16 +201,17 @@ public:
         cin.ignore(); // Limpiar el buffer de entrada antes de getline()
         getline(cin, nuevoSupervisor->nombre);
     
-        // Ingreso y validación de la fecha de supervisión
+        // Ingreso y validacion de la fecha de supervision
         bool fechaValida = false;
         do {
-            cout << "Ingrese la fecha de supervisión. Los dias deben estar entre 1-31 y los meses entre 1-12 (dd mm aaaa): ";
+            cout << "Ingrese la fecha de supervision. Los dias deben estar entre 1-31, los meses entre 1-12 y los anos deben tener 4 digitos (dd mm aaaa): ";
             if (cin >> nuevoSupervisor->dia >> nuevoSupervisor->mes >> nuevoSupervisor->ano) {
                 if (nuevoSupervisor->dia >= 1 && nuevoSupervisor->dia <= 31 &&
-                    nuevoSupervisor->mes >= 1 && nuevoSupervisor->mes <= 12) {
+                    nuevoSupervisor->mes >= 1 && nuevoSupervisor->mes <= 12 &&
+                    nuevoSupervisor->ano >= 0 && nuevoSupervisor->ano <= 9999) { 
                     fechaValida = true;
                 } else {
-                    cout << "Fecha invalida. Los dias deben estar entre 1-31 y los meses entre 1-12." << endl;
+                    cout << "Fecha invalida. Los dias deben estar entre 1-31, los meses entre 1-12 y los anos deben tener 4 digitos." << endl;
                 }
             } else {
                 cout << "Formato de fecha invalido. Intente nuevamente." << endl;
@@ -233,7 +234,6 @@ public:
         delete nuevoSupervisor; // Liberar la memoria asignada
     }
     
-
     void generarReporteTanques() {
         ofstream archivo("reporte_tanque.txt", ios::out);
 
